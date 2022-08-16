a11da9d5d8886bca48dbaf0f64f508418080a333
saugatdhimal <dhimalsaugat77@gmail.com>
2022-08-16 19:16:34 +0545

    Add exgaming repo

---
+++ config/config.js
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    title: "Ex Gaming",
+    titleTemplate: "Ex Gaming Zone - Community of Gamers",
+    description: "React Gatsby",
+    author: "Ex Gaming",
+    twitterUsername: "",
+    keywords: [
+        "bootstrap",
+        "html",
+        "react",
+        "gatsby",
+        "bonx",
+        "Gaming Website",
+    ],
+    image: "",
+    siteUrl: "https://bonx.com",
+    pathPrefix: "/",
+    localUrl: "http://localhost:8000",
+    canonical: "",
+    social: {
+        facebook: "https://www.facebook.com/",
+        twitter: "https://twitter.com/",
+        linkedin: "https://www.linkedin.com",
+    },
+    siteLanguage: "en",
+    favicon: "src/assets/images/favicon.png", // Used for manifest favicon generation
+    shortName: "Ex Gaming", // shortname for manifest. *Must* be shorter than 12 characters
+    themeColor: "#5E60E7",
+    backgroundColor: "#fff",
+};
